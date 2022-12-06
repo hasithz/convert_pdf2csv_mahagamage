7589885552f30a186ad79840edebe93c014d45e4
hasith <hasith.dhz@gmail.com>
2022-12-06 18:13:40 +0530

    final backend product

--- work/utils/struct_csv_data.py
+++ work/utils/struct_csv_data.py
@@ -1,12 +1,13 @@
 from .csv_struct import CSV_STRUCT
 import csv 
 
-def structure_csv_data(all_data:list):
+def structure_csv_data(all_data:list, ):
     splitted_data = []
     temp = []
     skip = False
     print(all_data[5][7])
     print(all_data[6][7])
+    count = 0
     for data in all_data:
         if data[7] == 'Booking Total':
             temp.append(data)
@@ -14,11 +15,12 @@ def structure_csv_data(all_data:list):
             # print(temp)
             structured_list = CSV_STRUCT(temp).set_Structure()
             # break
-            with open('test.csv', 'a+') as f:
+            with open(f'{count}.csv', 'a+') as f:
                 write = csv.writer(f)
                 write.writerows(structured_list)
                 # file.close()
             temp = []
+            count += 1
             # next(data)
             skip = True
             # check_null = 0
